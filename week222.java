15b92b53a9f874e92170adf3530be1c26b40df93
yy6951660-a11y <yy6951660@gmail.com>
2026-09-05 15:37:12 +0530

    Update week222.java

--- week222.java
+++ week222.java
@@ -1,1 +1,33 @@
 
+import java.util.Scanner;
+
+public class Main {
+
+    static String reverseEachWord(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder reversed = new StringBuilder(words[i]);
+            reversed.reverse();
+
+            result.append(reversed);
+
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String sentence = sc.nextLine();
+
+        System.out.println(reverseEachWord(sentence));
+
+        sc.close();
+    }
+}

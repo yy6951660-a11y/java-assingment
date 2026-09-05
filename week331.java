683b2305d483c1f88dda2b6adf1e02d771966736
yy6951660-a11y <yy6951660@gmail.com>
2026-09-05 14:34:36 +0530

    Create week331.java

---
+++ week331.java
@@ -0,0 +1,30 @@
+class PlacementRecord {
+    String studentName;
+    String company;
+    double packageLpa;
+
+    PlacementRecord(String studentName, String company, double packageLpa) {
+        this.studentName = studentName;
+        this.company = company;
+        this.packageLpa = packageLpa;
+    }
+
+    void printRecord() {
+        System.out.println(studentName + " -> " + company + " @ " + packageLpa + " LPA");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        PlacementRecord[] records = {
+            new PlacementRecord("Ravi", "TCS", 4.5),
+            new PlacementRecord("Anitha", "Zoho", 6.2),
+            new PlacementRecord("Karthik", "Infosys", 4.0)
+        };
+
+        for (PlacementRecord record : records) {
+            record.printRecord();
+        }
+    }
+}

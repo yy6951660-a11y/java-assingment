f3ab7ff3f074b4021f975d0c59be7e725bc60fd4
yy6951660-a11y <yy6951660@gmail.com>
2026-09-08 00:56:09 +0530

    Create week555.java

---
+++ week555.java
@@ -0,0 +1,76 @@
+final class LoanReceipt {
+
+    private final String memberId;
+    private final String[] bookIds;
+
+    public LoanReceipt(String memberId, String[] bookIds) {
+        this.memberId = memberId;
+        this.bookIds = bookIds.clone();
+    }
+
+    public String[] getBookIds() {
+        return bookIds.clone();
+    }
+
+    public LoanReceipt withCorrectedBookId(int index, String newId) {
+
+        String[] newBookIds = bookIds.clone();
+        newBookIds[index] = newId;
+
+        return new LoanReceipt(memberId, newBookIds);
+    }
+}
+
+
+class ReferenceOnlyLoanReceipt extends LoanReceipt {
+
+    private final String roomNumber;
+
+    public ReferenceOnlyLoanReceipt(
+            String memberId,
+            String[] bookIds,
+            String roomNumber) {
+
+        super(memberId, bookIds);
+        this.roomNumber = roomNumber;
+    }
+}
+
+
+class CirculationLedger {
+
+    static String branchCode;
+
+    static {
+        branchCode = "PT-BRANCH";
+    }
+
+    static String processNightlyCirculation(LoanReceipt[] receipts) {
+
+        int processed = 0;
+        int nullSkipped = 0;
+        int referenceOnly = 0;
+        int regular = 0;
+
+        for (LoanReceipt receipt : receipts) {
+
+            if (receipt == null) {
+                nullSkipped++;
+                continue;
+            }
+
+            processed++;
+
+            if (receipt instanceof ReferenceOnlyLoanReceipt) {
+                referenceOnly++;
+            } else {
+                regular++;
+            }
+        }
+
+        return processed + " processed | " +
+               nullSkipped + " null skipped | " +
+               referenceOnly + " reference-only | " +
+               regular + " regular";
+    }
+}
